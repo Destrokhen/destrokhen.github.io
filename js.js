789f7e8bebb9a8656a2b0512c61055f023f3e54b
destrokhen <armagiddon16@mail.ru>
2018-03-15 20:21:19 +0300

    inital commit

--- js.js
+++ js.js
@@ -1,148 +1,212 @@
 $(document).ready()
 { 
-		help1 = ['#s1','#s2','#s3','#s4','#s5','#s6','#s7','#s8','#s9','#s10','#s11','#s12','#s13','#s14'];
+		TimeCase = ['#s1','#s2','#s3','#s4','#s5','#s6','#s7','#s8','#s9','#s10','#s11','#s12','#s13','#s14'];
 		time = ['#time1','#time2',"#time3","#time4",'#time5','#time6','#time7','#time8','#time9','#time10','#time11','#time12','#time13','#time14'];
-		sd = ['#C1','#C2','#C3','#C4','#C5','#C6','#C7','#C8','#C9','#C10','#C11','#C12','#C13','#C14'];
-		dd = ['#D1','#D2','#D3','#D4','#D5','#D6','#D7','#D8','#D9','#D10','#D11','#D12','#D13','#D14']; 
-		help = {0:"24",1:"25",2:"26",3:"27",4:"28",5:"29",6:"30",7:"31",8:"32",9:"33"};
-        lil = ["#li1","#li2","#li3","#li4","#li5","#li6","#li7","#li8","#li9","#li10","#li11","#li12","#li13","#li14",];
+		TimeFist = ['#C1','#C2','#C3','#C4','#C5','#C6','#C7','#C8','#C9','#C10','#C11','#C12','#C13','#C14'];
+		TimeSecond = ['#D1','#D2','#D3','#D4','#D5','#D6','#D7','#D8','#D9','#D10','#D11','#D12','#D13','#D14']; 
+		TimeTranslet = {0:"24",1:"25",2:"26",3:"27",4:"28",5:"29",6:"30",7:"31",8:"32",9:"33"};
+        TitleTravel = ["#li1","#li2","#li3","#li4","#li5","#li6","#li7","#li8","#li9","#li10","#li11","#li12","#li13","#li14",];
         
         function elect(x1,x2)
         {
-            $("#Screen2").append('<iframe id="elke" frameborder="0" src="https://rasp.yandex.ru/informers/search/?fromId='+x1+'&amp;toId='+x2+'&amp;size=25&amp;color=1&amp;type=suburban"></iframe>');
+            $("#Screen2").append('<iframe id="elke" frameborder="1"sandbox="allow-forms"src="https://rasp.yandex.ru/informers/search/?fromId='+x1+'&amp;toId='+x2+'&amp;size=25&amp;color=1&amp;type=suburban"></iframe>');
             $('#choises').text("Расписание готово");
         }
+        
+        function TimeHelper (rasp,zapolnin,ChosTravel)
+        {
+                    var PointTime;
+                    var TimeSched;
+                    if (ChosTravel == true) 
+                    {
+                        PointTime = rasp[zapolnin].t;
+                    } else 
+                        {
+                            PointTime = rasp[zapolnin].ot;
+                        }
+                    if (PointTime == "")
+                        {
+                            TimeSched = 20145;
+                            return TimeHelper;
+                        }
+                    var HelpPer = PointTime.split(':');
+                    if (HelpPer[0][0] == "*")
+                        {
+                            var HelpStr = HelpPer[0].split('*');
+                            HelpPer[0] = HelpStr[1];
+                        }
+                    if (parseInt(HelpPer[0]) >= 0 && parseInt(HelpPer[0]) <= 9)
+                        {
+                            TimeSched = parseInt(TimeTranslet[parseInt(HelpPer[0])])*60 + parseInt(HelpPer[1]);
+                        }
+                    else
+                        {
+                            TimeSched = parseInt(HelpPer[0])*60 + parseInt(HelpPer[1]);
+                        }
+                    return TimeSched;
+        }
     
-		function ti(rasp,zapolnin,per)
+		function ti(rasp,zapolnin,ChosTravel)
 		{
-				function tizs(rasp,zapolnin,per)
-				{
-                        if (per == true) {var ra = rasp[zapolnin].t;} else {var ra = rasp[zapolnin].ot;}
-						hou = "",min = "";
-						var i = 0;
-                        if (ra[0] == '*' && ra[1] == "*") {i+=2;}
-						else if (ra[0] == "*"){i++;}
-						while (ra[i] != ":")
-						{hou += ra[i];i++;}
-						i++;
-						while (i != ra.length)
-								{min += ra[i];i++;}
-                        
-				}
-				
-				var hel = 0;
-				tizs(rasp,zapolnin,per);
-				while(hel != help1.length)
-					{
-						var date = new Date(),houn = date.getHours(),minn = date.getMinutes();
-						var ho = 0,mo = 0;
-						hou = parseInt(hou);
-						min = parseInt(min);
-						if (hou >= 0 && hou <= 9) {if(houn >= 0 && houn <= 9){ho = parseInt(help[hou]) - parseInt(help[houn]);} else {ho = parseInt(help[hou]) - houn;}} else {ho = parseInt(hou) - houn;}
-						if (ho > 0 && min - minn < 0){ho -=1;mo = (min+60) - minn;} else {mo = min - minn;}
-                        if (ho <= 0 && mo < 0){$(time[hel]).css("background-color","#F8F8F2");$(time[hel]).text("----------");$(time[hel]).css("color","#C1BDBD");}
-                        else if (ho == 0 && mo == 0) {$(time[hel]).css("background-color","#F71818");$(time[hel]).css("color","#FFF5F5");$(time[hel]).text("Сейчас");}
-                        else if (ho == 0 && mo <= 10) {$(time[hel]).css("background-color","#F71818");$(time[hel]).css("color","#FFF5F5");$(time[hel]).text("Через: "+mo.toString()+" Мин");}
-                        else if (ho == 0) {$(time[hel]).css("color","black");$(time[hel]).text("Через: "+mo.toString()+" Мин");$(time[hel]).css("background-color","#FFFFFF");}
-                        else if (ho > 0 && mo == 0) {$(time[hel]).css("color","black");$(time[hel]).text("Через: "+ho.toString()+" ч ");}
-                        else {$(time[hel]).text("Через: "+ho.toString()+" ч "+mo.toString()+" Мин");$(time[hel]).css("background-color","#FFFFFF");$(time[hel]).css("color","black");}
-						hel++;
-						zapolnin++;
-                        if (per == false) {var xal = 2;} else {var xal = 1;}
-                        if (zapolnin+2 == rasp.length+xal) {zapolnin = 0;}
-						tizs(rasp,zapolnin,per);
-					}
+            var i = 0;
+            while(i != TimeCase.length)
+                {
+                    var TimeSched = TimeHelper(rasp,zapolnin,ChosTravel);
+                    if (TimeSched > 1440)
+                        {
+                            TimeSched-= 1440;
+                        }
+                    var TimeNow = new Date().getHours() *60+new Date().getMinutes();
+                    var TimeNeed = TimeSched - TimeNow;
+                    if (TimeNeed < 0)
+                        {
+                            $(time[i]).text("");
+                            $(time[i]).css("color","black");
+                            $(TimeCase[i]).css("opacity","0.5");
+                        }
+                    else if (TimeNeed == 0)
+                        {
+                            $(time[i]).css("color","#F71818");
+                            $(time[i]).text("Сейчас");
+                            $(TimeCase[i]).css("opacity","1");
+                        }
+                    else if (TimeNeed <= 10)
+                        {
+                            $(time[i]).css("color","#F71818");
+                            $(time[i]).text(TimeNeed+" мин");
+                            $(TimeCase[i]).css("opacity","1");
+                        }
+                    else
+                        {
+                            var Min = TimeNeed % 60;
+                            var Hour = (TimeNeed - Min)/60;
+                            $(TimeCase[i]).css("opacity","1");
+                            if (Hour == 0)
+                                {
+                                    $(time[i]).text(Min.toString()+" мин");
+                                    $(time[i]).css("color","black");
+                                }
+                            else
+                                {
+                                    $(time[i]).text(Hour.toString()+' ч '+Min.toString()+" мин");
+                                    $(time[i]).css("color","black");
+                                }
+                        }
+                    i++;
+                    zapolnin++;
+                }
 		}
-		
-		function cheak(rasp,zapolnin,per)
-		{
-				function ctiz(rasp,zapolnin)
-				{
-				        if (per == true) {var ra = rasp[zapolnin].t;} else {var ra = rasp[zapolnin].ot;}
-						hou = "",min = "";
-						var i = 0;
-                        if (ra[0] == '*' && ra[1] == "*") {i+=2;}
-						else if (ra[0] == "*"){i++;}
-						while (ra[i] != ":")
-								{hou += ra[i];i++;}
-						i++;
-						while (i != ra.length)
-								{min += ra[i];i++;}
-				}
-				
-				var end = false;
-				ctiz(rasp,zapolnin);
-				while (end != true)
+    
+		function FindTime(rasp,zapolnin,ChosTravel)
+		{		
+				var End = false;
+				while (End != true)
 						{
-								var date = new Date(),houn = date.getHours(),minn = date.getMinutes();
-								hou = parseInt(hou);
-								min = parseInt(min);
-								if (hou - houn < 0)
-										{zapolnin++;ctiz(rasp,zapolnin);}
-								else if (hou - houn == 0 && min - minn < 0)
-										{zapolnin++;ctiz(rasp,zapolnin);}
-								else {end = true;}
+                            var TimeSched = TimeHelper(rasp,zapolnin,ChosTravel);
+                            if(TimeSched == 20145) 
+                                {
+                                    End = true;
+                                    zapolnin = 0;
+                                    continue;
+                                }
+                            var TimeNow = new Date().getHours() * 60 + new Date().getMinutes();
+                            if (TimeSched > 1440)
+                                {
+                                    TimeSched -= 1440;
+                                }
+                            if (TimeSched - TimeNow < 0)
+                                    {
+                                        var helpInt = (TimeSched - TimeNow)
+                                        if (helpInt >= -15)
+                                            {
+                                                End = true;
+                                            }
+                                        else
+                                            {
+                                                zapolnin++;  
+                                            }
+                                    }
+                            else {End = true;}
 						}
 				return zapolnin;
 		}
 		
-		function zap (rasp,zapolnin,per)
+		function TimePlace (rasp,zapolnin,ChosTravel)
 		{
-			var timeu = 20;
-			function tiz(rasp,zapolnin)
-				{
-						if (per == true) {var ra = rasp[zapolnin].t;} else {var ra = rasp[zapolnin].ot;}
-						hou = "",min = "";
-						var i = 0;
-                        if (ra[0] == "*" && ra[1] == "*") {i+=2;timeu = 50;}
-						else if (ra[0] == "*"){i++;timeu = 50;} else {timeu = 20;}
-						while (ra[i] != ":")
-						{hou += ra[i];i++;}
-						i++;
-						while (i != ra.length)
-								{min += ra[i];i++;}
-						hour = parseInt(hou);
-						mint = parseInt(min);
-						mint += timeu;
-						if (mint >= 60) {if (hour == 23){hour = 0;} else {hour += 1;} mint -= 60;}
+			var timeu = 20;				
+            var i = 0;
+            while (i != TimeCase.length)
+                {
+                    var PointTime;
+                    var Find = false;
+                    if (ChosTravel == true) {PointTime = rasp[zapolnin].t;} else {PointTime = rasp[zapolnin].ot;}
+                    if (PointTime == ""){zapolnin = 0;continue;}
+                    var HelpPer = PointTime.split(':');
+                    if (HelpPer[0][0] == "*")
+                        {
+                            Find = true;
+                            var HelpStr = HelpPer[0].split('*');
+                            HelpPer[0] = HelpStr[1];
+                        }
+                    var Hour = HelpPer[0];
+                    var Min = HelpPer[1];
                     
-				}
-				
-				var hel = 0;
-				tiz(rasp,zapolnin)
-				while (hel != help1.length)
-					{
-                        if (per == true) {var ra = rasp[zapolnin].t;} else {var ra = rasp[zapolnin].ot;}
-						$(sd[hel]).text(hou+':'+min);
-                        if (ra[0] == "*" && ra[1] == '*') {$(lil[hel]).text(" от ст.метро 'Славянский бульвар'");}
-                        else if (ra[0] == "*") {$(lil[hel]).text(" рейс до ст.метро 'Славянский бульвар'");} else 
-                        if (per == false){$(lil[hel]).text(" рейс от станции 'Одинцово'");} else {$(lil[hel]).text(" рейс до станции 'Одинцово'");}
-						if (mint >=0 && mint <= 9) {$(dd[hel]).text(hour.toString()+':'+"0"+mint.toString());}
-							else {$(dd[hel]).text(hour.toString()+":"+mint.toString());}
-						hel++;
-						zapolnin++;
-                        if (per == false) {var xal = 2;} else {var xal = 1;}
-                        if (zapolnin+2 == rasp.length+xal) {zapolnin = 0;}
-						tiz(rasp,zapolnin);
-					}
+                    var TimeSched;
+                    if (parseInt(Hour) >= 0 && parseInt(Hour) <= 9)
+                        {
+                            TimeSched = TimeTranslet[parseInt(Hour)]*60 + parseInt(Min);
+                        }
+                    else
+                        {
+                            TimeSched = parseInt(Hour)*60 + parseInt(Min);
+                        }
+                    TimeSched += timeu;
+                    var Min1 = TimeSched % 60;
+                    var Hour1 = (TimeSched - Min1) / 60;
+                    if (Min1 >= 0 && Min1 <= 9){Min1 = "0" + Min1;}
+                    $(TimeSecond[i]).text(Hour1+':'+Min1);
+                    $(TimeFist[i]).text(Hour+':'+Min);
+                    if (Find == true && ChosTravel == false) 
+                        {
+                            $(TitleTravel[i]).text('от ст.метро "Славянский бульвар"');
+                        }
+                    else if (Find == true && ChosTravel == true) 
+                        {
+                            $(TitleTravel[i]).text('рейс до ст.метро "Славянский бульвар"');
+                        } 
+                    else if (ChosTravel == false)
+                        {
+                            $(TitleTravel[i]).text('рейс от станции "Одинцово"');
+                        } else 
+                            {
+                                $(TitleTravel[i]).text('рейс до станции "Одинцово"');
+                            }
+                    i++;
+                    zapolnin++;
+                }
 				
 		}
-		
-        var now = new Date();
-        var day = now.getDay();
-        if (day >= 1 && day <= 5 ) {url = 'exsel_m_f.xlsx';}
-        else if (day == 6) {url = 'exsel_s.xlsx';} else {url = 'exsel_sa.xlsx';}
+    
+		// Main part;
+        var DayToday = new Date().getDay();
+        if (DayToday >= 1 && DayToday <= 5 ) {URL = 'exsel_m_f.xlsx';}
+        else if (DayToday == 6) {URL = 'exsel_s.xlsx';} else {URL = 'exsel_sa.xlsx';}
 		var oReq = new XMLHttpRequest();
-		oReq.open("GET", url , true);
+		oReq.open("GET", URL , true);
 		oReq.responseType = "arraybuffer";
+        // function pars
 		oReq.onload = function(e) 
 		{
 			var arraybuffer = oReq.response;
 
 			var data = new Uint8Array(arraybuffer);
 			var arr = new Array();
-			for(var i = 0;i != data.length;++i) {arr[i] = String.fromCharCode(data[i]);}
-			var bstr = arr.join("");
+			for(var i = 0;i != data.length;++i) 
+                {
+                    arr[i] = String.fromCharCode(data[i]);
+                }
+			
+            var bstr = arr.join("");
 
 			var workbool = XLSX.read(bstr,{type:"binary"});
 
@@ -151,41 +215,57 @@ $(document).ready()
 			var worksheet = workbool.Sheets[first_sheet_name];
 			var rasp = XLSX.utils.sheet_to_row_object_array(worksheet);
 			
-            var per = true;
-            var zapolnin = 0;
+            // MainCod
+            var ChosTravel = true;
             var poslet = true;
+            var HelpBool = true;
+            var zapolnin = 0;
+            zapolnin = FindTime(rasp,zapolnin,ChosTravel);
+            TimePlace(rasp,zapolnin,ChosTravel);
+            
             $('#ot').css("border","1px solid red");
-            var helppr = true;
-            setInterval(function()
+            jQuery('.buus').bind('click',function ()
+                {
+                    zapolnin = 0;
+                    if($(this).attr('id') == "do")
                         {
-                 jQuery('.buus').bind('click',function ()
-                    {
-                        var id = $(this).attr('id');
-                        if(id == "do"){$('#ot').css("border","1px solid black");$('#do').css("border","1px solid red");} else {$('#do').css("border","1px solid black");$('#ot').css("border","1px solid red");}
-                        if (id == 'ot') {per = true;if(helppr == true){x1 = 's9600721';$('#choises').text("Выберите станцию прибытия (От Одинцово)");}}
-                        else if (id == "do"){per = false;if (helppr){x2 = 's9600721';$('#choises').text("Выберите станцию отправления (До Одинцово)");}}
-                        zapolnin = cheak(rasp,zapolnin,per);
-                        zap(rasp,zapolnin,per);
-                    });
-                        },1000);
+                            $('#ot').css("border","0");
+                            $('#do').css("border","1px solid red");
+                            ChosTravel = false;
+                            if (HelpBool)
+                                {
+                                    x2 = 's9600721';
+                                    $('#choises').text("Выберите станцию отправления (До Одинцово)");
+                                }
+                        } else 
+                            {
+                                $('#do').css("border","0");
+                                $('#ot').css("border","1px solid red");
+                                ChosTravel = true;
+                                if (HelpBool)
+                                {
+                                    x1 = 's9600721';
+                                    $('#choises').text("Выберите станцию прибытия (От Одинцово)");
+                                }
+                            }
+                    zapolnin = FindTime(rasp,zapolnin,ChosTravel);
+                    TimePlace(rasp,zapolnin,ChosTravel);
+                });
                         $('.elc').bind('click',function ()
                             {
-                                if (per == true){x1 = "s9600721";x2 = $(this).attr('id');} else {x2 = 's9600721';x1 = $(this).attr('id');}
+                                if (ChosTravel == true)
+                                    {
+                                        x1 = "s9600721";
+                                        x2 = $(this).attr('id');
+                                    } else 
+                                        {
+                                            x2 = 's9600721';
+                                            x1 = $(this).attr('id');
+                                        }
                                 $(".non").remove();
                                 elect(x1,x2);
-                                helppr = false;
                             });
-            var cll = true;
-            $('.Settings').bind('click',function()
-            {
-                if (cll == true){$('#manescreen').append("<div id ='set'><p>Настройки</p><p><input id = 'checkbox' type='checkbox'>Круто, что нажал , но тут ничего нет(нажми опять на Шестерёнку)</p>");cll = false;} else {$('#set').remove();cll = true;}
-            });
-			zapolnin = cheak(rasp,zapolnin,per);
-			var xl = zapolnin;
-			zap(rasp,zapolnin,per);
-			setInterval(function(){ti(rasp,xl,per);},100);
-			$("#checkbox").click(function(){if ($(this).is(':checked')){alert("asdasda");}})
+			setInterval(function(){ti(rasp,zapolnin,ChosTravel);},100);
 		}
-		oReq.send();
-		
+		oReq.send();	
 }
